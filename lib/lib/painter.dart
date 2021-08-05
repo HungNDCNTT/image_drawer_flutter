98c1458dc3add0ec4d20fbeea1d9aa557e1d4538
HungND <hungnd@newwave.vn>
2021-08-05 15:32:26 +0700

    fix

--- lib/lib/painter.dart
+++ lib/lib/painter.dart
@@ -409,6 +409,9 @@ class _PainterState extends State<Painter> {
       image.copy(paths.path + '/' + DateTime.now().millisecondsSinceEpoch.toString() + '.png');
       print('Image link: $image');
       widget.calBackImage == null ? print('nothing') : widget.calBackImage(image);
+      setState(() {
+        isEnableDoneBtn = !isEnableDoneBtn;
+      });
     }).catchError((onError) {
       print(onError);
     });
